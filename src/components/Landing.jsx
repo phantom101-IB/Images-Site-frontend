96bec69a23792b1ea35bd51efe5d1cf6a9f920a9
phantom101-IB <mi95920@gmail.com>
2025-01-01 16:09:24 +0000

    Update CSS

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,5 @@
 const Landing = () => {
+    const token = localStorage.getItem("token")
     return <div className="landing-div">
         <div className="landing-control-div">
             <div className="text-div">
@@ -6,7 +7,9 @@ const Landing = () => {
                     <h1>Discover and share best moments and photos</h1>
                     <p>Get inspired with incredible photos from diveese styles and genre around the world</p>
                     <div className="landing-buttons">
-                        <button className="SignUp lnd-btn"> Sign Up </button>
+                        {
+                            !token && <button className="SignUp lnd-btn"> Sign Up </button>
+                        }
                         <button className="Explore lnd-btn"> Explore </button>
                     </div>
                 </div>
