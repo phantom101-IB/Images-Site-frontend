ccb4992159881177ef2e600f0701f8a279e1cb52
phantom101-IB <mi95920@gmail.com>
2025-01-04 21:35:30 +0000

    User profile Development

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,7 +1,27 @@
+import { FaUpload } from "react-icons/fa"
+
 const UserProfile = () => {
-    return <section>
-        <div>
-            <h2>User Profile div</h2>
+    return <section className="main_profile">
+        <div className="profile_control">
+            <div className="user_photos"></div>
+            <div className="user_profile">
+                <div className="user_profile_ctrl">
+                    <div className="user_profile_pic">
+                        <div className="pro_pic-div">
+                            <img src="./puppie6.jpg" alt="User_profile" />
+                        </div>
+                        <div className="user_followers">
+                            <li>Followers</li>
+                            <li>Likes</li>
+                            <li>Following</li>
+                        </div>
+                    </div>
+                    <div className="upload_class">
+                        <button className="upload_btn">Upload <FaUpload /> </button>
+
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 }
